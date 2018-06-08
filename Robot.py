feece29cab533b34e6c14443bfe01740d24d8214
MrTroble <13779831+MrTroble@users.noreply.github.com>
2018-06-08 18:51:57 +0200

    Inital commit

---
+++ Robot.py
@@ -0,0 +1,74 @@
+import RPi.GPIO as GPIO
+
+
+class Robot:
+
+    # static class members
+    Stop: int = 0
+    Frequency: int = 20
+    DutyCycle: int = 20
+    MotorDC_A: int = 30
+    MotorDC_B: int = 30
+
+    def __init__(self):
+
+        # Pin vars
+        motor_af = 10
+        motor_ab = 9
+        motor_bf = 8
+        motor_bb = 7
+
+        # Setmode for pins
+        GPIO.setmode(GPIO.BCM)
+
+        # Setup pins
+        GPIO.setup([motor_af, motor_ab, motor_bf, motor_bb], GPIO.OUT)
+
+        # Setup PWM for pins
+        self.pwm_AF = GPIO.PWM(motor_af, Robot.Frequency)
+        self.pwm_AB = GPIO.PWM(motor_ab, Robot.Frequency)
+        self.pwm_BF = GPIO.PWM(motor_bf, Robot.Frequency)
+        self.pwm_BB = GPIO.PWM(motor_bb, Robot.Frequency)
+
+        # Start duty cycle with none
+        self.pwm_AF.start(Robot.Stop)
+        self.pwm_AB.start(Robot.Stop)
+        self.pwm_BF.start(Robot.Stop)
+        self.pwm_BB.start(Robot.Stop)
+
+    # ===============================================
+    # Robot control section
+    # ===============================================
+
+    def stop(self):
+        self.pwm_AF.ChangeDutyCycle(Robot.Stop)
+        self.pwm_AB.ChangeDutyCycle(Robot.Stop)
+        self.pwm_BF.ChangeDutyCycle(Robot.Stop)
+        self.pwm_BB.ChangeDutyCycle(Robot.Stop)
+
+    def forward(self):
+        self.pwm_AF.ChangeDutyCycle(Robot.MotorDC_A)
+        self.pwm_AB.ChangeDutyCycle(Robot.Stop)
+        self.pwm_BF.ChangeDutyCycle(Robot.MotorDC_B)
+        self.pwm_BB.ChangeDutyCycle(Robot.Stop)
+
+    def back(self):
+        self.pwm_AF.ChangeDutyCycle(Robot.Stop)
+        self.pwm_AB.ChangeDutyCycle(Robot.MotorDC_A)
+        self.pwm_BF.ChangeDutyCycle(Robot.Stop)
+        self.pwm_BB.ChangeDutyCycle(Robot.MotorDC_B)
+
+    def left(self):
+        self.pwm_AF.ChangeDutyCycle(Robot.Stop)
+        self.pwm_AB.ChangeDutyCycle(Robot.MotorDC_A)
+        self.pwm_BF.ChangeDutyCycle(Robot.MotorDC_B)
+        self.pwm_BB.ChangeDutyCycle(Robot.Stop)
+
+    def right(self):
+        self.pwm_AF.ChangeDutyCycle(Robot.MotorDC_A)
+        self.pwm_AB.ChangeDutyCycle(Robot.Stop)
+        self.pwm_BF.ChangeDutyCycle(Robot.Stop)
+        self.pwm_BB.ChangeDutyCycle(Robot.MotorDC_B)
+
+    def close(self):
+        GPIO.cleanup()
---
+++ test.py
@@ -0,0 +1,24 @@
+import Robot
+import time
+
+robot = Robot()
+
+time.sleep(1)
+
+robot.forward()
+
+time.sleep(1)
+
+robot.back()
+
+time.sleep(1)
+
+robot.right()
+
+time.sleep(0.5)
+
+robot.left()
+
+time.sleep(0.5)
+
+robot.close()
