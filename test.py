12bedfa02bf9f5f22106bb679cab498952c78f0b
MrTroble <13779831+MrTroble@users.noreply.github.com>
2018-07-05 16:54:42 +0200

    add warnings

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import time
 
 print("Initialize robot API")
 # Initialize Robot.py API
-robot = Robot.Robot(4, 22, 24)
+robot = Robot.Robot(4, 23, 22)
 print("Finished initializing")
 
 # Test logic
